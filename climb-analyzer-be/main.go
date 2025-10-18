7dbe91fa47742f41925eed57c685ab9939ba5eb9
Jan Vaclavik <janvaclavik@valachnet.cz>
2025-10-18 22:17:06 +0200

    Dockerfile for backend added

--- climb-analyzer-be/main.go
+++ climb-analyzer-be/main.go
@@ -113,9 +113,27 @@ func recoveryMiddleware(c *gin.Context) {
 }
 
 func main() {
+	isProduction := false
+	backendUrl := "localhost:8080"
+
+	_, defined := os.LookupEnv("PRODUCTION")
+	if defined {
+		isProduction = true
+	}
+
+	if isProduction {
+		gin.SetMode(gin.ReleaseMode)
+		backendUrl = "0.0.0.0:8080"
+	}
+
 	router := gin.Default()
-	router.Use(cors.Default())
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:4200", "http://localhost"}
+	config.AllowMethods = []string{"POST", "OPTIONS"}
+	router.Use(cors.New(config))
+
 	router.Use(recoveryMiddleware)
-	router.POST("/analyze", analyzeClimbs)
-	router.Run("localhost:8080")
+	router.POST("/api/analyze", analyzeClimbs)
+	router.Run(backendUrl)
 }
